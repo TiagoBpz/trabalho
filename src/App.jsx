8a55e512a2f924d1150900ab87048af47c7a90e6
TiagoBpz <tiagobrasileiroplt@gmail.com>
2025-04-14 16:38:10 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <>
-     <Menu option01='Sessão 01' option03='Mapa e contato'/>
+     <Menu option01='Sessão 01' option02='Sessão 02' option03='Mapa e contato' option04='Cálculo'/>
      <main>
       <section id='s1' className={style.s1}>
         {/* <img src={img01} alt="o amor esta no ar" width={200} height={"auto"}/> */}
@@ -46,6 +46,7 @@ function App() {
           )
         })}
       </section>
+      
      {/* <section id='s3'>
         <h2>CONTATO</h2>
         <br />
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import { Analytics } from "@vercel/analytics/react"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Calcs from './calcs.jsx';
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/contact",
     element: <Contact/>,
+  },
+  {
+    path: "/calcs",
+    element: <Calcs/>,
   },
 ]);
 
